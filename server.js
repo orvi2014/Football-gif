17cb04b45c8be61d3ba9a18e3624ded412d196b1
orvi2014 <das.orvi@yahoo.com>
2018-09-17 19:01:00 +0600

    get json data and related activity

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const express = require('express');
 const request = require('request');
 const app = express()
 
-const apiKey = '*****************';
-
 app.use(express.static('public'));
 app.set('view engine', 'ejs')
 // focusing on root directory
@@ -15,13 +13,22 @@ app.get('/', function (req, res) {
   // http://api.giphy.com/v1/gifs/search?q=soccer&api_key=bClDpdztYdNCVxLgdB5heqgwY9eG5ODP&limit=5
   let apiKey = 'bClDpdztYdNCVxLgdB5heqgwY9eG5ODP';
   let q = 'soccer';
-  let url = 'http://api.giphy.com/v1/gifs/search?q=soccer&api_key=bClDpdztYdNCVxLgdB5heqgwY9eG5ODP&limit=5';
+  let url = `http://api.giphy.com/v1/gifs/search?q=${q}&api_key=${apiKey}&limit=5`;
 
   request(url, function (err, response, body) {
   if(err){
     console.log('error:', error);
   } else {
-    console.log('body:', body);
+    // json parsing
+    let gif = JSON.parse(body)
+    // dictionary data
+    let data = gif.data
+    // iterate over dictionary
+    for(var i in data){
+      console.log("source:", data[i].url);
+    }
+    let gif_show = `${gif.data.source}`
+    console.log('body:', gif);
   }
 });
 })
