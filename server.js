c60e984ff5a68a5fffb3e4db9257114060838dcc
orvi2014 <das.orvi@yahoo.com>
2018-09-17 16:56:08 +0600

    giphy api key url added

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 // importing modules
-const express = require('express')
+const express = require('express');
+const request = require('request');
 const app = express()
+
+const apiKey = '*****************';
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs')
-
 // focusing on root directory
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  // search api method giphy
+  //http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5
+  // http://api.giphy.com/v1/gifs/search?q=soccer&api_key=bClDpdztYdNCVxLgdB5heqgwY9eG5ODP&limit=5
+  let apiKey = 'bClDpdztYdNCVxLgdB5heqgwY9eG5ODP';
+  let q = 'soccer';
+  let url = 'http://api.giphy.com/v1/gifs/search?q=soccer&api_key=bClDpdztYdNCVxLgdB5heqgwY9eG5ODP&limit=5';
+
+  request(url, function (err, response, body) {
+  if(err){
+    console.log('error:', error);
+  } else {
+    console.log('body:', body);
+  }
+});
 })
 // creating a server
 app.listen(3000, function () {
